b3c0234b36219fe53bd2c4d1c12d17d2fdd40ec6
edsonfervo <edson55_@hotmail.com>
2024-07-01 17:07:35 -0500

    Update crypto utils

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,26 +1,27 @@
 const CryptoJS = require("crypto-js");
+const { utf8_to_b64, b64_to_utf8 } = require("./base64");
 
 const CRYPTO_PASSWORD = process.env.CRYPTO_PASSWORD;
 const CRYPTO_SALT = process.env.CRYPTO_SALT;
-const CRYPTO_KEYSIZE = process.env.CRYPTO_KEYSIZE;
-const CRYPTO_ITERATIONS = process.env.CRYPTO_ITERATIONS;
+const CRYPTO_KEYSIZE = parseInt(process.env.CRYPTO_KEYSIZE, 10);
+const CRYPTO_ITERATIONS = parseInt(process.env.CRYPTO_ITERATIONS, 10);
 
 const crypto_encrypt = (str) => {
   let bytes = CryptoJS.PBKDF2(CRYPTO_PASSWORD, CRYPTO_SALT, {
-    keySize: CRYPTO_KEYSIZE,
+    keySize: CRYPTO_KEYSIZE / 32,
     iterations: CRYPTO_ITERATIONS,
   });
 
   let iv = CryptoJS.enc.Hex.parse(bytes.toString().slice(0, 32));
   let key = CryptoJS.enc.Hex.parse(bytes.toString().slice(32, 96));
 
-  let ciphertext = CryptoJS.AES.encrypt(str, key, { iv });
+  let ciphertext = CryptoJS.AES.encrypt(utf8_to_b64(str), key, { iv });
   return ciphertext.toString();
 };
 
 const crypto_decrypt = (str) => {
   let bytes = CryptoJS.PBKDF2(CRYPTO_PASSWORD, CRYPTO_SALT, {
-    keySize: CRYPTO_KEYSIZE,
+    keySize: CRYPTO_KEYSIZE / 32,
     iterations: CRYPTO_ITERATIONS,
   });
   let iv = CryptoJS.enc.Hex.parse(bytes.toString().slice(0, 32));
@@ -28,7 +29,7 @@ const crypto_decrypt = (str) => {
 
   let ciphertext = CryptoJS.AES.decrypt(str, key, { iv });
   let txt = ciphertext.toString(CryptoJS.enc.Utf8);
-  return txt;
+  return b64_to_utf8(txt);
 };
 
 module.exports = { crypto_encrypt, crypto_decrypt };
